eac404d5f1c6fe8409e25a8c4415e32979140017
fuyuanshun <fuyuanshun@gmail.com>
2021-05-08 14:20:27 +0800

    init commit

--- src/main/java/com/fys/leetcode/Main.java
+++ src/main/java/com/fys/leetcode/Main.java
@@ -3,6 +3,10 @@ package com.fys.leetcode;
 public class Main {
     /**
      * 1. 两数之和
+     * @param arr 整数数组
+     * @param target 整数目标值
+     * @return
+     *
      * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
      *
      * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
@@ -26,4 +30,33 @@ public class Main {
         }
         return result;
     }
+
+    /**
+     * 2. 两数相加
+     * @param listNode1 链表1
+     * @param listNode2 链表2
+     * @return
+     *
+     * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+     *
+     * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+     *
+     * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/add-two-numbers
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public ListNode addTwoNumbers(ListNode listNode1, ListNode listNode2) {
+
+        return null;
+    }
+
+    public class ListNode {
+       int val;
+       ListNode next;
+       ListNode() {}
+       ListNode(int val) { this.val = val; }
+       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+   }
 }
