681f912955325a0898645023e9a00a39c7f264af
fuyuanshun <fuyuanshun@gmail.com>
2021-05-08 13:43:35 +0800

    Initial commit

---
+++ src/main/java/com/fys/leetcode/Main.java
@@ -0,0 +1,29 @@
+package com.fys.leetcode;
+
+public class Main {
+    /**
+     * 1. 两数之和
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+     *
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+     *
+     * 你可以按任意顺序返回答案。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int[] twoSum(int[] arr, int target){
+        int[] result = new int[2];
+        flag:for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break flag;
+                }
+            }
+        }
+        return result;
+    }
+}
